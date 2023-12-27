a0afd82f100276b2b9b536cd48acd0c21a4c0354
Edwin Hernandez <edwinhern.16@gmail.com>
2023-12-27 09:08:43 -0600

    Delete unused files and dependencies

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,24 +1,10 @@
-import { Button } from "@/components/ui/button";
 import { ModeToggle } from "@/components/ui/mode-toggle";
-import { SearchInput } from "@/components/ui/search-input";
-import useCounter from "@/stores/useCounter";
 
 export function HomePage() {
-  const { count, increase, decrease } = useCounter();
-
   return (
-    <>
-      <div className="flex h-screen flex-col items-center justify-center gap-4">
-        <h2 className="text-red-500">{count}</h2>
-        <div className="flex flex-row gap-4">
-          <Button onClick={increase}>Increase</Button>
-          <Button variant={"outline"} onClick={decrease}>
-            Decrease
-          </Button>
-          <ModeToggle />
-        </div>
-        <SearchInput aria-placeholder="search" />
-      </div>
-    </>
+    <div className="flex h-screen flex-col items-center justify-center gap-4">
+      <h2>Hello from Edwin!</h2>
+      <ModeToggle />
+    </div>
   );
 }
--- src/providers/index.tsx
+++ src/providers/index.tsx
@@ -1,13 +1,9 @@
-import { Toaster } from "@/components/ui/toaster";
 import { ThemeProvider } from "@/providers/theme-provider";
 
 export const Providers: React.FC<ChildProps> = ({ children }) => {
   return (
-    <>
-      <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
-        {children}
-        <Toaster />
-      </ThemeProvider>
-    </>
+    <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
+      {children}
+    </ThemeProvider>
   );
 };
