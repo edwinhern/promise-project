92f13d6a7eb5cd3afc475b000be7a18e9c6bbfb7
Edwin Hernandez <edwinhern.16@gmail.com>
2023-12-28 22:51:28 -0600

    Update routes.tsx file

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -2,15 +2,15 @@ import { createRoutesFromElements, Route } from 'react-router-dom';
 import { createBrowserRouter } from 'react-router-dom';
 
 import { Base } from '@/layouts/Base';
-import { HomePage } from '@/pages/Home';
+// import { HomePage } from '@/pages/Home';
 import { NotFoundPage } from '@/pages/NotFound';
 import { ThreePage } from '@/pages/Three';
 
 export const AppRoutes = createBrowserRouter(
   createRoutesFromElements(
     <Route element={<Base />}>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/three" element={<ThreePage />} />
+      <Route path="/" element={<ThreePage />} />
+      {/* <Route path="/three" element={<ThreePage />} /> */}
       <Route path="*" element={<NotFoundPage />} />
     </Route>
   )
